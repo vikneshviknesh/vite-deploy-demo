e4081ff0b88bd2f9bf0c5afa75e2698ba57b9a66
Vigneshwaran <v.vigneshwaran@starhealth.in>
2022-10-13 14:22:59 +0530

    * Counter page created with styles * useCounter - Custom hook added

---
+++ src/src/CustomHooks/useCounter.js
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+export function useCounter() {
+    const [count, setCount] = useState(0);
+
+    const increment = () => {
+        setCount(prevCount => prevCount + 1)
+    }
+
+    const decrement = () => {
+        setCount(prevCount => prevCount - 1)
+    }
+    return { count, increment, decrement }
+}
